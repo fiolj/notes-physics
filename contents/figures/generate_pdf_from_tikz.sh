fc093da24c85daba1ef98409217e7ee267609bbd
Juan Fiol <fiol@cab.cnea.gov.ar>
2016-05-06 16:52:27 -0300

    Recuperé viejas figuras

--- contents/figures/generate_pdf_from_tikz.sh
+++ contents/figures/generate_pdf_from_tikz.sh
@@ -1,1 +1,1 @@
-pdflatex -jobname $1 "\documentclass{standalone}\usepackage{tkz-graph,amsmath}\begin{document}\input{$1.tikz}\end{document}" && convert -density 150 $1.pdf -quality 90 $1_p.png
+pdflatex -jobname $1 "\documentclass{standalone}\usepackage{tkz-graph,amsmath,bm}\begin{document}\newcommand{\vect}[1]{\bm{#1}}\input{$1.tikz}\end{document}" && convert -density 150 $1.pdf -quality 90 $1_p.png
