a3867e29e4ea5bf594bac145e8d21f7fffaa7cbd
Juan Fiol <juanfiol@gmail.com>
2016-05-26 12:43:16 -0300

    Arreglos para html y formación de clusters
    Hice arreglos para que las figuras compilaran en el html
    Inicié la corrección de cálculos de "reaction rates" para dimerización

--- contents/figures/generate_pdf_from_tikz.sh
+++ contents/figures/generate_pdf_from_tikz.sh
@@ -1,1 +1,3 @@
-pdflatex -jobname $1 "\documentclass{standalone}\usepackage{tkz-graph,amsmath,bm}\begin{document}\newcommand{\vect}[1]{\bm{#1}}\input{$1.tikz}\end{document}" && convert -density 150 $1.pdf -quality 90 $1_p.png
+pdflatex -jobname $1 "\documentclass{standalone}\usepackage{tkz-graph,amsmath,bm}\begin{document}\newcommand{\vect}[1]{\bm{#1}}\input{$1.tikz}\end{document}"
+ # && convert -density 150 $1.pdf -quality 90 $1.png
+latex -jobname $1 "\documentclass{standalone}\usepackage{tkz-graph,amsmath,bm}\begin{document}\newcommand{\vect}[1]{\bm{#1}}\input{$1.tikz}\end{document}" && dvips $1.dvi
